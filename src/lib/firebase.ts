cec290ee85a137204bb0364aa0ae26f3bb41bef1
chris-m-bernard <chrisbernard11@icloud.com>
2025-11-11 12:46:41 -0500

    removed old, unused code from firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,6 @@
 // src/lib/firebase.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -16,5 +14,3 @@ const firebaseConfig = {
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-// export const db = getFirestore(app);
-// export const storage = getStorage(app);
